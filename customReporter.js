601c990168b8609323b91cfc753f89156cfdc4c4
Caio Borghi <caiohoborghi@gmail.com>
2023-07-30 14:05:38 -0300

    update customReporter to use asyncGenerator

--- customReporter.js
+++ customReporter.js
@@ -1,32 +1,27 @@
-import { Transform } from 'node:stream';
-
-const customReporter = new Transform({
-  writableObjectMode: true,
-  transform(event, encoding, callback) {
+export default async function* customReporter(source) {
+  for await (const event of source) {
     switch (event.type) {
       case 'test:start':
-        throw new Error("This reporter has failed");
-        callback(null, `test ${event.data.name} started\n`);
+        throw new Error("Custom reporter error")
+        yield `test ${event.data.name} started\n`;
         break;
       case 'test:pass':
-        callback(null, `test ${event.data.name} passed\n`);
+        yield `test ${event.data.name} passed\n`;
         break;
       case 'test:fail':
-        callback(null, `test ${event.data.name} failed\n`);
+        yield `test ${event.data.name} failed\n`;
         break;
       case 'test:plan':
-        callback(null, 'test plan\n');
+        yield 'test plan';
         break;
       case 'test:diagnostic':
-        callback(null, event.data.message + '\n');
+        yield `${event.data.message}\n`;
         break;
       case 'test:coverage': {
         const { totalLineCount } = event.data.summary.totals;
-        callback(null, `total line count: ${totalLineCount}\n`);
+        yield `total line count: ${totalLineCount}\n`;
         break;
       }
     }
-  },
-});
-
-export default customReporter;
+  }
+}
